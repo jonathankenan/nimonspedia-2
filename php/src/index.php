a501618bfa61ae03b27028d8fcdccffa25a36d33
TKurr <theokurniady12@gmail.com>
2025-10-14 11:46:44 +0700

    test: index.php connect

--- php/src/index.php
+++ php/src/index.php
@@ -1,4 +1,29 @@
-<?php
-
-echo 'Hello World!';
-echo '<script src="/public/index.js"></script>';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Nimonspedia - Milestone 1</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background: #f9f9f9;
+      text-align: center;
+      margin-top: 10%;
+      color: #333;
+    }
+    .box {
+      background: white;
+      display: inline-block;
+      padding: 30px 50px;
+      border-radius: 15px;
+      box-shadow: 0 0 15px rgba(0,0,0,0.1);
+    }
+  </style>
+</head>
+<body>
+  <div class="box">
+    <h1>Test Test Test Sahur</h1>
+    <?php include_once __DIR__ . '/app/config/db.php'; ?>
+  </div>
+</body>
+</html>
